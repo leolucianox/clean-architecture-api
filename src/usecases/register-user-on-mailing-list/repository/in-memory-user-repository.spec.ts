a1e69500ff8e8f55bd26b46ac4610a7f4a0b9c90
leolucianox <leonardo.silva@zappts.com>
2020-12-08 22:23:13 -0300

    test: return all users in the repository

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.spec.ts
@@ -5,17 +5,24 @@ describe('In memory User respository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
     const userRepo = new InMemoryUserRepository(users)
-    const user = await userRepo.findUserByEmail('any@email.com')
+    const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user).toBeNull()
   })
 
   test('should return user if it is found in the repository', async () => {
     const users: UserData[] = []
     const name = 'any_name'
-    const email = 'any@email.com'
+    const email = 'any@mail.com'
     const userRepo = new InMemoryUserRepository(users)
     await userRepo.add({ name, email })
-    const user = await userRepo.findUserByEmail('any@email.com')
+    const user = await userRepo.findUserByEmail('any@mail.com')
     expect(user.name).toBe('any_name')
+  })
+
+  test('should return all users in the repository', async () => {
+    const users: UserData[] = [{ name: 'any_name', email: 'any@mail.com' }, { name: 'second_name', email: 'second@mail.com' }]
+    const userRepo = new InMemoryUserRepository(users)
+    const returnedUsers = userRepo.findAllUsers()
+    expect((await returnedUsers).length).toBe(2)
   })
 })
