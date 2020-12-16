ab573208824f89e2579ed2f49a34d91503c11e07
leolucianox <leonardo.silva@zappts.com>
2020-12-15 23:12:16 -0300

    test: validate emails with domain parts larger than 63 chars

--- src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
+++ src/usecases/register-user-on-mailing-list/entities/email-validation.spec.ts
@@ -35,4 +35,14 @@ describe('Email validation', () => {
     const email = '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
+
+  test('should not accept empty domain', () => {
+    const email = 'any@'
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept doamin with a part larger than 63 chars', () => {
+    const email = 'any@' + 'd'.repeat(64) + '.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
